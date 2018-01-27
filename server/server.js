4c29b9c438df69453fb8a8505d2336bc423e89ba
Taran Gill <gill.tarandeep@yahoo.ca>
2018-01-27 17:07:19 -0500

    Complete login system

--- server/server.js
+++ server/server.js
@@ -14,23 +14,46 @@ mongoose.connect('mongodb://localhost:27017/presentRDB');
 const app = express();
 
 app.set(`view engine`, `hbs`);
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res) => {
   res.render('../views/login.hbs');
 });
 
-app.post('/userTable', (req, res) => {
+app.get('/canvas', (req, res) => {
+  res.render('../views/canvas.hbs');
+})
+
+app.post('/loginAttempt', (req, res) => {
+  User.findOne({'username': req.body.username, 'password': req.body.password}, (err, success) => {
+    if (err) {
+      res.redirect('/');
+    }
+    else {
+      res.redirect('/canvas');
+    }
+  });
+
+});
+
+app.get('/registrationScreen', (req, res) => {
+  res.render('../views/registration.hbs');
+})
+
+app.post('/register', (req, res) => {
+  console.log(req.body)
   var user = new User({
     username: req.body.username,
     password: req.body.password
   });
   user.save().then((doc) => {
-    res.send(doc);
+    res.redirect('/');
   }, (e) => {
     res.status(400).send(e);
   });
 });
+
+// app.post('/')
 
 app.listen(port, () => {
   console.log("Started on port ", port);
--- server/userModel.js
+++ server/userModel.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-var x = 5;
 let User = mongoose.model(`User`, {
   username: {
     type: String,
-    required: true,
+    // required: true,
     minlength: 4
   },
   password: {
     type: String,
-    required: true
+    // required: true
   }
 });
 
